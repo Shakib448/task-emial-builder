8f567bc3ac8ca256283377d0a3673e09a55fd270
Shakiba448 <shakiba448@gmail.com>
2021-03-27 21:54:19 +0600

    WIP on Builder

--- src/Components/Builder/Builder.js
+++ src/Components/Builder/Builder.js
@@ -2,14 +2,55 @@ import React, { useRef } from "react";
 import EmailEditor from "react-email-editor";
 import "./CustomBuilder";
 import sample from "./sample.json";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  height: 100%;
+`;
+
+const Bar = styled.div`
+  flex: 1;
+  background-color: #61dafb;
+  color: #000;
+  padding: 10px;
+  display: flex;
+  max-height: 40px;
+  h1 {
+    flex: 1;
+    font-size: 16px;
+    text-align: left;
+  }
+  button {
+    flex: 1;
+    padding: 10px;
+    margin-left: 10px;
+    font-size: 14px;
+    font-weight: bold;
+    background-color: #000;
+    color: #fff;
+    border: 0px;
+    max-width: 150px;
+    cursor: pointer;
+  }
+`;
 const Builder = () => {
   const emailEditorRef = useRef(null);
+
+  const saveDesign = () => {
+    emailEditorRef.current.editor.saveDesign((design) => {
+      console.log("saveDesign", design);
+      alert("Design JSON has been logged in your developer console.");
+    });
+  };
 
   const exportHtml = () => {
     emailEditorRef.current.editor.exportHtml((data) => {
       const { design, html } = data;
       console.log("exportHtml", html);
+      alert("Output HTML has been logged in your developer console.");
     });
   };
 
@@ -26,13 +67,18 @@ const Builder = () => {
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={exportHtml}>Export HTML</button>
-      </div>
+    <Container>
+      <Bar>
+        <h1>React Email Editor (Demo)</h1>
 
-      <EmailEditor ref={emailEditorRef} onLoad={onLoad} />
-    </div>
+        <button onClick={saveDesign}>Save Design</button>
+        <button onClick={exportHtml}>Export HTML</button>
+      </Bar>
+
+      <React.StrictMode>
+        <EmailEditor ref={emailEditorRef} onLoad={onLoad} />
+      </React.StrictMode>
+    </Container>
   );
 };
 
