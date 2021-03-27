c9599e4d7f6a3b8ad5fb307d82615bdb77ca01ee
Shakiba448 <shakiba448@gmail.com>
2021-03-27 21:37:44 +0600

    WIP on Builder

---
+++ src/Components/Builder/Builder.js
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import EmailEditor from "react-email-editor";
+
+const Builder = () => {
+  const emailEditorRef = useRef(null);
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data) => {
+      const { design, html } = data;
+      console.log("exportHtml", html);
+    });
+  };
+
+  const onLoad = () => {
+    // you can load your template here;
+    // const templateJson = {};
+    // emailEditorRef.current.editor.loadDesign(templateJson);
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={exportHtml}>Export HTML</button>
+      </div>
+
+      <EmailEditor ref={emailEditorRef} onLoad={onLoad} />
+    </div>
+  );
+};
+
+export default Builder;
